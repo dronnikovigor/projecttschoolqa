37ac3abb86e72bf76345584fcbdff317f8f201f3
Yulia Polushina <287jpi@gmail.com>
2019-03-17 18:08:30 +0300

    created TestBase

--- src/test/java/SiteTest.java
+++ src/test/java/SiteTest.java
@@ -1,37 +1,16 @@
-import common.Env;
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import steps.StepLogin;
 import steps.StepSignUp;
 
-import java.io.File;
-import java.util.concurrent.TimeUnit;
-
 
 @DisplayName("Site Test")
-class SiteTest {
-    private static WebDriver driver;
-
-    @BeforeEach
-    void setup() {
-        File file;
-        if (Env.OS.equals("windows")) {
-            file = new File("drivers/chromedriver.exe");
-            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-        } else {
-            file = new File("drivers/chromedriver");
-            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
-        }
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-    }
+class SiteTest extends TestBase {
 
     @Test
     @DisplayName("Login Test Case")
     void testCase1() {
-        StepLogin loginPageSteps = new StepLogin(driver);
+        StepLogin loginPageSteps = new StepLogin(getDriver());
         loginPageSteps.execFillInLogin("ivan_88");
         loginPageSteps.execFillInPwd("4321");
         loginPageSteps.execClickLogin();
@@ -40,10 +19,10 @@ class SiteTest {
     @Test
     @DisplayName("SignUp Test Case")
     void testCase2() {
-        StepLogin loginPageSteps = new StepLogin(driver);
+        StepLogin loginPageSteps = new StepLogin(getDriver());
         loginPageSteps.execClickSignUp();
 
-        StepSignUp stepSignUp = new StepSignUp(driver);
+        StepSignUp stepSignUp = new StepSignUp(getDriver());
         stepSignUp.execFillInUsername("123452");
         stepSignUp.execFillInEmail("123@123.ru");
         stepSignUp.execFillInPwd("123456");
@@ -58,8 +37,4 @@ class SiteTest {
         stepSignUp.execClickNextPersonal();
     }
 
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-    }
 }
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,41 @@
+import common.Env;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+@DisplayName("Test Base")
+public class TestBase {
+    private static WebDriver driver;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+    @BeforeEach
+    void setup() {
+        File file;
+        if (Env.OS.equals("windows")) {
+            file = new File("drivers/chromedriver.exe");
+            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        } else {
+            file = new File("drivers/chromedriver");
+            System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        }
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+    }
+
+}
